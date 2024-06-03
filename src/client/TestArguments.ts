eb1ece99ec5ce301f01c3dd864f5a613c39ad3d0
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-03 12:06:06 +0900

    test: Arguments를 테스트하였다

---
+++ src/client/TestArguments.ts
@@ -0,0 +1,16 @@
+interface tempObj {
+  a : string
+  b : string
+}
+
+//* arguemnts의 활용은 any타입으로 명시되어 있다. 
+//* 사용하면 안될듯함 :)                              
+function ForOfArguments(...args : tempObj[]){
+  const arrArgs = arguments
+
+  for(const obj of arguments){
+    for(const property in obj){
+      console.log(`${property} : ${obj[property]}`);
+    }
+  }
+}
